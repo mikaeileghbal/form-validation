7397bdc3ee3d00e511d2a4d41a32bc7996aa6dc3
Mikaeil <mikaeileghbal@yahoo.com>
2022-05-04 11:29:45 +0430

    Add validation to different input types

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,45 @@
-const formValidate = (function () {
+const Validator = (function () {
+  const inputTypes = {
+    text: {
+      pattern: "[a-zA-Z]",
+      messages: ["is required", "must be text characters only"],
+    },
+    email: {
+      pattern: "([a-zA-Z]|[0-9])+@([a-zA-Z]|[0-9])+\\.([a-zA-Z]){3}",
+      messages: ["is required", "must be a valid email address"],
+    },
+    tel: {
+      pattern: "\\+?([0-9]){8}",
+      messages: ["is required", "must be a valid phone number"],
+    },
+    number: {
+      pattern: "([0-9])",
+      messages: ["is required", "must be a numbers only"],
+    },
+    password: {
+      pattern: ".{8}",
+      messages: ["is required", "at least 8 characters"],
+    },
+  };
+
+  function validate(input, type) {
+    let errorCode = -1;
+    if (input.value === "" || input.value === "null") {
+      return inputTypes[type].messages[0];
+    }
+    const pattern = new RegExp(inputTypes[type].pattern);
+    if (!pattern.test(input.value)) {
+      return inputTypes[type].messages[1];
+    }
+    return errorCode;
+  }
+
+  return {
+    validate,
+  };
+})();
+
+const FormValidate = (function (document) {
   let currentTab = 0;
   showTab(currentTab);
 
@@ -6,7 +47,6 @@ const formValidate = (function () {
     const tabs = document.querySelectorAll(".tab");
     console.log(tabIndex);
     tabs[tabIndex].style.display = "block";
-
     if (tabIndex === 0) {
       document.querySelector("#previous").style.display = "none";
     } else {
@@ -22,30 +62,23 @@ const formValidate = (function () {
 
   function previousNext(stepIndex) {
     const tabs = document.querySelectorAll(".tab");
-
     if (stepIndex === 1 && !validateFormTab()) {
       return;
     }
-
     tabs[currentTab].style.display = "none";
-
     currentTab = currentTab + stepIndex;
-
     if (currentTab >= tabs.length) {
       submitForm();
       return;
     }
-
     showTab(currentTab);
   }
 
   function setStepsIndicator(stepIndex) {
     const steps = document.querySelectorAll(".step");
-
     steps.forEach((step) => {
       step.classList.remove("active");
     });
-
     steps[stepIndex].classList.add("active");
   }
 
@@ -53,7 +86,6 @@ const formValidate = (function () {
     let valid = true;
     const tab = document.querySelectorAll(".tab")[currentTab];
     const inputs = tab.querySelectorAll("input");
-
     inputs.forEach((input) => {
       const errorCode = Validator.validate(input, input.type);
       if (errorCode !== -1) {
@@ -64,16 +96,14 @@ const formValidate = (function () {
         input.classList.remove("invalid");
       }
     });
-
     if (valid) {
       document.querySelectorAll(".step")[currentTab].classList.add("finish");
     }
-
     return valid;
   }
 
   function submitForm() {
-    document.querySelector("#multiPageForm").submitForm();
+    document.querySelector("#multiPageForm").submit();
   }
 
   document.querySelector("#previous").addEventListener("click", (e) => {
@@ -89,35 +119,4 @@ const formValidate = (function () {
       validateFormTab(e);
     });
   });
-})();
-
-const Validator = (function () {
-  const inputTypes = {
-    text: {
-      pattern: "[a-zA-Z]",
-      messages: ["is required", "must be text characters only"],
-    },
-    email: {
-      pattern: "([a-zA-Z]|[0-9])+@([a-zA-Z]|[0-9])+.(com|org|net)",
-      messages: ["is required", "must be a valid email address"],
-    },
-  };
-
-  function validate(input, type) {
-    let errorCode = -1;
-
-    if (input.value === "" || input.value === "null") {
-      return inputTypes[type].messages[0];
-    }
-
-    const pattern = new RegExp(inputTypes[type].pattern);
-    if (!pattern.test(input.value)) {
-      return inputTypes[type].messages[1];
-    }
-
-    return errorCode;
-  }
-  return {
-    validate,
-  };
-})();
+})(document);
